4c1bbbe7d342afbdeaef8e53e3f7f2a8c918b8e3
Vertika <75982385+VertikaD@users.noreply.github.com>
2021-02-05 20:05:23 +0530

    Add files via upload

---
+++ Practice/Python/Tuples.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+    # tuple takes iterable and here we are passing a list to tuple .
+    t = tuple(integer_list)
+    print(hash(t))
