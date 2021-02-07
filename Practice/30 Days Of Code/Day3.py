a16627258ffddcb75ffc0ddaa4a93231844357c1
Vertika <75982385+VertikaD@users.noreply.github.com>
2021-02-07 17:44:58 +0530

    Add files via upload

---
+++ Practice/30 Days Of Code/Day3.py
@@ -0,0 +1,12 @@
+# Intro to Conditional Statements.
+if __name__ == '__main__':
+    N = int(input())
+    if (N % 2 != 0):
+        print('Weird')
+    elif (N % 2 == 0):
+        if N >= 2 and N <= 5:
+            print('Not Weird')
+        if N >= 6 and N <= 20:
+            print('Weird')
+        if N > 20:
+            print('Not Weird')
