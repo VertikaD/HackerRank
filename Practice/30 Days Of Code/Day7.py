45a729e4b2235a96fad047b79f7b9f5a2abe66a2
Vertika <75982385+VertikaD@users.noreply.github.com>
2021-02-08 13:10:30 +0530

    Add files via upload

---
+++ Practice/30 Days Of Code/Day7.py
@@ -0,0 +1,8 @@
+# Arrays
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+    print(arr)
+    reverse_array = list(reversed(arr))
+    [print(x, end=' ') for x in reverse_array]
